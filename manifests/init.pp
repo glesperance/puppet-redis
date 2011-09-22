1e18cf793bb10865152549f60a4ead883b6eab33
Gabriel Lesperance <gabriel@wavo.me>
2011-09-22 17:35:43 -0400

    tests

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,13 +3,13 @@ import "defines/*.pp"
 class redis($enable_service = true) {
     
     user { "redis":
-      ensure => present
+      ensure => present,
     }
     
     redis_source { git: 
         owner           => "redis",
         group           => "redis",
         require         => User["redis"],
-        enable_service  => $enable_service
+        enable_service => $enable_service
     }
 }
