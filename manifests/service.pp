3eeb4ae5b6f211f3827128f176998bf37064dda7
Gabriel Lesperance <gabriel@wavo.me>
2015-01-28 14:32:16 -0500

    MODIFIED - configuration file management

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,23 +1,35 @@
 define redis::service(
-    $name = 'server', 
-    $config_file = '/etc/redis.conf',
-    $enable_service = true, 
-    $version = '2.2.13',
+    $enable_service = true,
+
     $path = '/usr/local/src',
     $bin = '/usr/local/bin',
+
     $owner = 'redis',
     $group = 'redis',
-    $db_folder  = '/var/lib/redis'
+
+    $redis_service_name = 'redis', 
+    $redis_port         = 6379,
+    $redis_maxmemory    = '500MB',
+    $redis_appendonly   = 'yes',
+
+    $config_file = "/etc/${redis_service_name}.conf",
+    $db_folder  = "/var/lib/${redis_service_name}"
 ) {
-  
-  $full_name = "redis-${name}"
-  
-  file { "/etc/init.d/${full_name}":
+
+  file { "/etc/init.d/${redis_service_name}":
     content => template("redis/redis-server.erb"),
     owner   => root,
     group   => root,
     mode    => 744,
-    notify  => Service[$full_name]
+    notify  => Service[$redis_service_name]
+  }
+
+  file { $config_file:
+    content => template('redis/redis.conf.erb'),
+    owner   => root,
+    group   => root,
+    mode    => 744,
+    notify  => Service[$redis_service_name]
   }
   
   file { $db_folder:
@@ -26,11 +38,11 @@ define redis::service(
     , group   => $group
   }
   
-  service { $full_name:
+  service { $redis_service_name:
       enable      => $enable_service
     , ensure      => $enable_service
     , hasrestart  => true
-    , require     => [File["/etc/init.d/${full_name}"], Class['redis'], File[$db_folder]]
+    , require     => [File["/etc/init.d/${redis_service_name}"], File[$config_file], Class['redis'], File[$db_folder]]
   }
 
 }
